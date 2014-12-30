617d815ed63e3189acd1ba7e211ea4a29f5ece4d
Jesper Sahlin <jesper.sahlin@gmail.com>
2014-12-30 13:09:53 +0100

    DesignView Updayte 1

--- HyperChess/app/src/main/java/hyperchessab/hyperchess/Designer.java
+++ HyperChess/app/src/main/java/hyperchessab/hyperchess/Designer.java
@@ -1,18 +1,50 @@
 package hyperchessab.hyperchess;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 
 /**
  * Created by jespe_000 on 2014-12-30.
  */
 public class Designer extends Game {
 
+    Drawable[][] tiles;
+    final int width = 3, height = 3;
+    private static final int tileSize = 10;
+
     public Designer(Context context, Camera camera){
         super(context, camera);
-        board = new GameBoard(context);
-        this.camera = camera;
+//        board = new GameBoard(context, 3, 3);
+        tiles = new Drawable[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = context.getResources().getDrawable(R.drawable.tile_shape);
+                tiles[x][y].setBounds(x * tileSize,y * tileSize,x + tileSize, y + tileSize);
+            }
+        }
     }
 
+    @Override
+    public void Update(double dt) {
+        super.Update(dt);
+        if(InputData.Clicked){
+            Point p = InputData.ClickPoint;
+            int i = p.x;
+        }
+    }
 
-
+    @Override
+    public void Draw(Canvas c) {
+        c.setMatrix(camera.getTransform());
+        c.drawColor(Color.WHITE);
+        //board.Draw(c);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y].draw(c);
+            }
+        }
+    }
 }
